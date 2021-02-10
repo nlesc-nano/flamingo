c33177211f246be51148eda6c8ce2db58101407a
felipez <tifonzafel@gmail.com>
2021-02-10 17:44:57 +0100

    implementing filter with single functional group #43

--- flamingo/screen.py
+++ flamingo/screen.py
@@ -14,7 +14,7 @@ import sys
 from functools import partial
 from multiprocessing import Pool
 from pathlib import Path
-from typing import Any, FrozenSet, List, Mapping, Optional, Tuple
+from typing import Any, Set, List, Mapping, Optional, Tuple
 
 import numpy as np
 import pandas as pd
@@ -115,8 +115,7 @@ def apply_filters(molecules: pd.DataFrame, opts: Options, output_file: Path) -> 
         "include_functional_groups": include_functional_groups,
         "exclude_functional_groups": exclude_functional_groups,
         "bulkiness": filter_by_bulkiness,
-        "scscore": filter_by_scscore,
-        "single_anchor": filter_single_anchor,
+        "scscore": filter_by_scscore
         }
 
     for key in opts.filters.keys():
@@ -132,24 +131,42 @@ def apply_filters(molecules: pd.DataFrame, opts: Options, output_file: Path) -> 
 
 def include_functional_groups(molecules: pd.DataFrame, opts: Options) -> pd.DataFrame:
     """Check that the molecules contain some functional groups."""
-    groups = opts["filters"]["include_functional_groups"]
+    groups = opts["filters"]["include_functional_groups"]["groups"]
+    maximum = opts["filters"]["include_functional_groups"]["maximum"]
     logger.debug(f"including molecules that contains the groups: {groups}")
-    return filter_by_functional_group(molecules, opts, "include_functional_groups", False)
+    if maximum == 1:
+        return filter_single_group(molecules, groups, False)
+    else:
+        return filter_by_functional_group(molecules, groups, False)
 
 
 def exclude_functional_groups(molecules: pd.DataFrame, opts: Options) -> pd.DataFrame:
     """Check that the molecules do not contain some functional groups."""
-    groups = opts["filters"]["exclude_functional_groups"]
+    groups = opts["filters"]["exclude_functional_groups"]["groups"]
+    maximum = opts["filters"]["exclude_functional_groups"]["maximum"]
     logger.debug(f"exclude molecules that contains the groups: {groups}")
-    return filter_by_functional_group(molecules, opts, "exclude_functional_groups", True)
+    return filter_by_functional_group(molecules, groups, True)
 
 
-def filter_by_functional_group(molecules: pd.DataFrame, opts: Options, key: str,
-                               exclude: bool) -> pd.DataFrame:
+def filter_single_group(
+    molecules: pd.DataFrame, groups: List[str], exclude: bool) -> pd.DataFrame:
+    """Check that the molecule has a single functional group."""
+    # First filter the molecules that have the target functional groups
+    molecules = filter_by_functional_group(molecules, groups, False)
+
+    # Now remove the molecules that have more than 2 different functional groups
+    patterns = {Chem.MolFromSmarts(f) for f in groups}
+    pattern_check = np.vectorize(partial(has_single_substructure, patterns))
+    molecules = molecules[pattern_check(molecules["rdkit_molecules"])]
+
+    # Finally exclude molecules that have more that 2 identical functional groups
+    return exclude_same_functional_groups(molecules, patterns)
+
+def filter_by_functional_group(
+    molecules: pd.DataFrame, groups: List[str], exclude: bool) -> pd.DataFrame:
     """Search for a set of functional_groups."""
     # Transform functional_groups to rkdit molecules
-    functional_groups = opts["filters"][key]
-    patterns = {Chem.MolFromSmarts(f) for f in functional_groups}
+    patterns = {Chem.MolFromSmarts(f) for f in groups}
 
     # Function to apply predicate
     pattern_check = np.vectorize(partial(has_substructure, patterns))
@@ -162,23 +179,42 @@ def filter_by_functional_group(molecules: pd.DataFrame, opts: Options, key: str,
     return molecules[has_pattern]
 
 
-def has_substructure(patterns: FrozenSet, mol: Chem.Mol) -> bool:
+def exclude_same_functional_groups(molecules: pd.DataFrame, patterns: Set[Chem.rdchem.Mol]) -> pd.DataFrame:
+    """Exclude molecules that has the same functional group more than one time."""
+    has_carboxylic_acid = has_substructure(patterns, Chem.MolFromSmiles("[OH]C=O"))
+
+    # Select the functional groups in patterns
+    pairs = [(has_carboxylic_acid, Fragments.fr_COO)]
+    # Check that the functional group is present and select its fragment counter
+    all_counters = map(lambda t: t[1], filter(lambda t: t[0], pairs))
+
+    # Iterate over all functional groups
+    for counter_function in all_counters:
+        function_fragments = np.vectorize(counter_function)
+        # Count the number of a specific functional group in a molecule
+        number_of_groups = function_fragments(molecules["rdkit_molecules"])
+        # Keep only molecules with a single functional group
+        molecules = molecules[number_of_groups == 1]
+    return molecules
+
+
+
+
+def has_substructure(patterns: Set[Chem.rdchem.Mol], mol: Chem.Mol) -> bool:
     """Check if there is any element of `pattern` in `mol`."""
     return False if mol is None else any(mol.HasSubstructMatch(p) for p in patterns)
 
-
-def filter_single_anchor(molecules: pd.DataFrame, opts: Options) -> pd.DataFrame:
-    """Exclude molecules that containing more that a single functional group used as anchor."""
-    logger.debug(f"exclude molecules with more than a single {opts.anchor} anchoring group")
-    anchor = Chem.MolFromSmiles(opts.anchor)
-    if anchor.HasSubstructMatch(Chem.MolFromSmarts("[OH]C=O")):
-        fun_fragment = np.vectorize(Fragments.fr_COO)
-    else:
-        msg = f"single_anchor_filter not implemented for anchor: {opts.anchor}"
-        raise NotImplementedError(msg)
-
-    singles = fun_fragment(molecules["rdkit_molecules"])
-    return molecules[singles == 1]
+def has_single_substructure(patterns: Set, mol: Chem.Mol) -> bool:
+    """Check if there a single functional pattern in mol."""
+    acc = 0
+    for pat in patterns:
+        has_pattern = mol.HasSubstructMatch(pat)
+        if has_pattern and acc == 0:
+            acc += 1
+        # There is more than one pattern
+        elif has_pattern and acc > 0:
+            return False
+    return True
 
 
 def filter_by_bulkiness(molecules: pd.DataFrame, opts: Options) -> pd.DataFrame:
